7fef16ec96c4e3cdca47e1e8fe65d7dedf2b6c6c
dae-seung han <ips3471@naver.com>
2022-02-10 01:27:11 +0900

    set initial global style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app.tsx';
-import '@fortawesome/fontawesome-free/js/all.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './app.tsx'
+import '@fortawesome/fontawesome-free/js/all.js'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './assets/styles/theme'
+import GlobalStyle from './assets/styles/global-styles'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
