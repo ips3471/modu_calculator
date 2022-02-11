073855b01177a87a0bdcadd370b89ac7e4bfebb4
dae-seung han <ips3471@naver.com>
2022-02-11 17:48:39 +0900

    add Action component

---
+++ src/components/select-action.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import ButtonComponent from './button';
+
+const Container = styled.ul`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	height: 5rem;
+`;
+
+type ActionProps = {};
+
+function Action({}: ActionProps) {
+	return (
+		<Container>
+			<ButtonComponent
+				icon={<i className='fas fa-circle-play'></i>}
+				name='구매'
+				callback={() => {
+					console.log('buttonClick');
+				}}
+			></ButtonComponent>
+			<ButtonComponent
+				icon={<i className='fas fa-circle-pause'></i>}
+				name='지불'
+				callback={() => {
+					console.log('buttonClick');
+				}}
+			></ButtonComponent>
+			<ButtonComponent
+				icon={<i className='fas fa-circle-dot'></i>}
+				name='인수'
+				callback={() => {
+					console.log('buttonClick');
+				}}
+			></ButtonComponent>
+			<ButtonComponent
+				icon={<i className='fas fa-circle-stop'></i>}
+				name='매각'
+				callback={() => {
+					console.log('buttonClick');
+				}}
+			></ButtonComponent>
+		</Container>
+	);
+}
+
+export default Action;
