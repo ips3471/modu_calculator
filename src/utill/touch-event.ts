086dab54891a9db30d184e2a69513b7371031b03
dae-seung han <ips3471@naver.com>
2022-03-16 20:15:53 +0900

    add manifest and maskable icons

--- src/utill/touch-event.ts
+++ src/utill/touch-event.ts
@@ -1,20 +1,19 @@
 import { UpdatingState } from './../assets/interfaces/interfaces';
 class TouchEvent {
 	private readonly longEnough: number;
-	private tick: number = 300;
 	private timer: ReturnType<typeof setInterval> | undefined;
 	constructor(longEnough: number = 300) {
 		this.longEnough = longEnough;
 	}
 
 	touchStart(callback: UpdatingState<boolean>) {
-		this.tick = this.longEnough;
+		let tick = this.longEnough;
 		this.timer = setInterval(() => {
-			if (this.tick <= 0) {
+			if (tick <= 0) {
 				this.touchEnd();
 				callback(true);
 			} else {
-				this.tick -= 100;
+				tick -= 100;
 			}
 		}, 100);
 	}
